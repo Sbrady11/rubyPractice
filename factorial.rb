c1b193e3412c52c8cbd932c62c982f0c5dfe50d9
Simon Brady <simonbrady@mac.com>
2017-12-20 16:52:38 -0500

    Create factorial.rb

---
+++ factorial.rb
@@ -0,0 +1,10 @@
+# Write a method that takes an integer `n` in; it should return
+# n*(n-1)*(n-2)*...*2*1. Assume n >= 0.
+#
+# As a special case, `factorial(0) == 1`.
+#
+# Difficulty: easy.
+
+def factorial(n)
+  (1..n).inject(:*) || 1
+end
