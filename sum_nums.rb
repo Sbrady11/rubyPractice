0dc8496ca85a038d504fdf942fa99a16f05a15a8
Simon Brady <simonbrady@mac.com>
2017-12-22 17:49:22 -0500

    Create sum_nums.rb

---
+++ sum_nums.rb
@@ -0,0 +1,8 @@
+# Write a method that takes in an integer `num` and returns the sum of
+# all integers between zero and num, up to and including `num`.
+#
+# Difficulty: easy.
+
+def sum_nums(num)
+  (0..num).inject(:+)
+end
